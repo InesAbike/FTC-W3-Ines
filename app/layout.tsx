c06bba5f4c2c6aa2376152467bd166c03c332d37
GitOmonwale <inesagbozo@gmail.com>
2025-09-11 23:04:41 +0100

    feat: enhanced  UI components

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,31 +10,31 @@ const gilroy = localFont({
     // Regular
     { path: '../public/fonts/Gilroy-Regular.woff2', weight: '400', style: 'normal' },
     { path: '../public/fonts/Gilroy-RegularItalic.woff2', weight: '400', style: 'italic' },
-    
+
     // Medium
     { path: '../public/fonts/Gilroy-Medium.woff2', weight: '500', style: 'normal' },
     { path: '../public/fonts/Gilroy-MediumItalic.woff2', weight: '500', style: 'italic' },
-    
+
     // Semibold
     { path: '../public/fonts/Gilroy-Semibold.woff2', weight: '600', style: 'normal' },
     { path: '../public/fonts/Gilroy-SemiboldItalic.woff2', weight: '600', style: 'italic' },
-    
+
     // Bold
     { path: '../public/fonts/Gilroy-Bold.woff2', weight: '700', style: 'normal' },
     { path: '../public/fonts/Gilroy-BoldItalic.woff2', weight: '700', style: 'italic' },
-    
+
     // ExtraBold
     { path: '../public/fonts/Gilroy-Extrabold.woff2', weight: '800', style: 'normal' },
     { path: '../public/fonts/Gilroy-ExtraboldItalic.woff2', weight: '800', style: 'italic' },
-    
+
     // Black
     { path: '../public/fonts/Gilroy-Black.woff2', weight: '900', style: 'normal' },
     { path: '../public/fonts/Gilroy-BlackItalic.woff2', weight: '900', style: 'italic' },
-    
+
     // Light
     { path: '../public/fonts/Gilroy-Light.woff2', weight: '300', style: 'normal' },
     { path: '../public/fonts/Gilroy-LightItalic.woff2', weight: '300', style: 'italic' },
-    
+
     // Thin
     { path: '../public/fonts/Gilroy-Thin.woff2', weight: '100', style: 'normal' },
     { path: '../public/fonts/Gilroy-ThinItalic.woff2', weight: '100', style: 'italic' },
@@ -59,13 +59,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${gilroy.variable} antialiased text-primary-dark-blue`}
+        className={`${gilroy.variable} antialiased text-primary-dark-blue bg-white`}
       >
-                <CurrencyProvider>
+        <CurrencyProvider>
 
-        <ConditionalNavbar />
-        {children}
-        <Footer />
+          <ConditionalNavbar />
+          {children}
+          <Footer />
         </CurrencyProvider>
       </body>
     </html>
