44d9f41a525c9bf3a8458930a31f370329bb08c1
GitOmonwale <inesagbozo@gmail.com>
2025-09-16 01:04:35 +0100

    feat: enhanced  navbar

--- app/components/category/Dogs.tsx
+++ app/components/category/Dogs.tsx
@@ -103,9 +103,6 @@ const SmallDogs = () => {
                                     Pet Sellers
                                 </h2>
                             </div>
-                            <div className='flex items-end'>
-                                Dropdownm
-                            </div>
                         </div>
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
                                 {pets.map((pet, index) => (
